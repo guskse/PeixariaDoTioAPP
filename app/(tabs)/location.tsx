73774c244ae6d19f53b02c5c35879b75f6967832
gustavo <guskse@gmail.com>
2026-05-05 11:44:00 -0400

    added location component and layouts, added icons and map

--- app/(tabs)/location.tsx
+++ app/(tabs)/location.tsx
@@ -1,4 +1,12 @@
-import { Button, Linking, Text, View } from "react-native";
+import {
+  Button,
+  Image,
+  Linking,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function Location() {
   const openMap = () => {
@@ -6,11 +14,86 @@ export default function Location() {
   };
 
   return (
-    <View style={{ padding: 16, flex: 1 }}>
-      <Text style={{ flex: 1 }}>
-        Endereço: Cidade Nova II, Av. Bpo Massa 116, 69095-160.
+    <View style={styles.container}>
+      <View style={styles.iconsContainer}>
+        <Image
+          source={require("../../assets/images/tambaqui-icon.png")}
+          style={styles.icon}
+        />
+        <Image
+          source={require("../../assets/images/pacu-icon.png")}
+          style={styles.icon}
+        />
+        <Image
+          source={require("../../assets/images/sardinha-icon.png")}
+          style={styles.icon}
+        />
+      </View>
+      <TouchableOpacity onPress={openMap} activeOpacity={0.8}>
+        <Image
+          source={require("../../assets/images/mapa.png")}
+          style={styles.map}
+        />
+      </TouchableOpacity>
+
+      <Text style={styles.address}>
+        📍 Cidade Nova II, Av. Bpo Massa 116, 69095-160
       </Text>
-      <Button title="Abrir no mapa" onPress={openMap} />
+      <Text style={styles.hours}>
+        🕒 Funcionamento: 11:00 às 15:00
+        {"\n"}🚫 Fechado às segundas-feiras
+      </Text>
+
+      <View style={styles.button}>
+        <Button title="Abrir no mapa" onPress={openMap} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "flex-start",
+  },
+
+  map: {
+    width: "100%",
+    height: 200,
+    borderRadius: 16,
+    marginBottom: 12,
+  },
+
+  address: {
+    fontSize: 16,
+    marginBottom: 16,
+  },
+
+  hours: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#52796F",
+    marginBottom: 16,
+  },
+
+  button: {
+    borderRadius: 10,
+    overflow: "hidden",
+    marginTop: 30,
+  },
+
+  icon: {
+    width: 80,
+    height: 80,
+    resizeMode: "contain",
+  },
+
+  iconsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    marginTop: 20,
+    marginBottom: 60,
+  },
+});
